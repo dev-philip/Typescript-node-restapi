6580fac4154aa4267d9a805254213d7b65d0e884
Phillip Awobusuyi <phillip.awobusuyi@ndmhospitality.com>
2022-09-15 16:18:36 -0400

    Login processes

--- src/service/user.service.ts
+++ src/service/user.service.ts
@@ -13,3 +13,26 @@ export async function createUser(input: DocumentDefinition<UserDocument>) {
 export async function findUser(query: FilterQuery<UserDocument>) {
   return User.findOne(query).lean();
 }
+
+
+export async function validatePassword({
+  email,
+  password,
+}: {
+  email: UserDocument["email"];
+  password: string;
+}) {
+  const user = await User.findOne({ email });
+
+  if (!user) {
+    return false;
+  }
+
+  const isValid = await user.comparePassword(password);
+
+  if (!isValid) {
+    return false;
+  }
+
+  return omit(user.toJSON(), "password");
+}
